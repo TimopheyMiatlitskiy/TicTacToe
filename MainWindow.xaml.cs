a04f92a7b9042b2c9936cf6913b1c070180ee463
Timax <timax.met@gmail.com>
2024-04-13 21:31:26 +0300

    adding animate

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 
 namespace TicTacToe
 {
@@ -20,11 +23,13 @@ namespace TicTacToe
             {
                 if (playerX)
                 {
-                    button.Content = "X";
+                    DrawX(button);
+                    button.Tag = "X";
                 }
                 else
                 {
-                    button.Content = "O";
+                    DrawO(button);
+                    button.Tag = "O";
                 }
 
                 // Проверка на победу
@@ -96,6 +101,57 @@ namespace TicTacToe
                     button.Content = null;
                 }
             }
+        }
+        private async void DrawX(Button button)
+        {
+            Grid grid = new Grid();
+            grid.Width = button.ActualWidth;
+            grid.Height = button.ActualHeight;
+
+            Line line1 = new Line();
+            line1.Stroke = Brushes.Red;
+            line1.StrokeThickness = 5;
+            line1.X1 = 0;
+            line1.Y1 = 0;
+            line1.X2 = 0;
+            line1.Y2 = 0;
+            grid.Children.Add(line1);
+
+            Line line2 = new Line();
+            line2.Stroke = Brushes.Red;
+            line2.StrokeThickness = 5;
+            line2.X1 = button.ActualWidth;
+            line2.Y1 = 0;
+            line2.X2 = button.ActualWidth;
+            line2.Y2 = 0;
+            grid.Children.Add(line2);
+
+            button.Content = grid;
+
+            // Анимация рисования крестика
+            DoubleAnimation line11Animate = new DoubleAnimation(0, button.ActualWidth, TimeSpan.FromSeconds(1));
+            line1.BeginAnimation(Line.X2Property, line11Animate);
+            DoubleAnimation line12Animate = new DoubleAnimation(0, button.ActualHeight, TimeSpan.FromSeconds(1));
+            line1.BeginAnimation(Line.Y2Property, line12Animate);
+
+            DoubleAnimation line21Animate = new DoubleAnimation(button.ActualWidth, 0, TimeSpan.FromSeconds(1));
+            line2.BeginAnimation(Line.X1Property, line21Animate);
+            DoubleAnimation line22Animate = new DoubleAnimation(0, button.ActualHeight, TimeSpan.FromSeconds(1));
+            line2.BeginAnimation(Line.Y1Property, line22Animate);
+
+            await Task.Delay(1000); // Задержка для завершения анимации
+        }
+
+        private void DrawO(Button button)
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Stroke = Brushes.Blue;
+            ellipse.Fill = Brushes.White;
+            ellipse.Width = button.ActualWidth - 20;
+            ellipse.Height = button.ActualHeight - 20;
+            ellipse.Margin = new Thickness(5);
+
+            button.Content = ellipse;
         }
     }
 }
